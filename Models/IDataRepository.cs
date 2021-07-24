4b369359c1ae2672ee000e760e21a2dbf2e10a01
Artyom <artemvoitenko58@gmail.com>
2021-07-24 20:36:29 +0300

    Added initial models and changed Startup

---
+++ Models/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRMSystem.Models
+{
+    public interface IDataRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllData();
+        Task<T> GetDataById(int dataId);
+        Task CreateData(T data);
+        Task UpdateDataById(int dataId);
+        Task DeleteDataById(int dataId);
+    }
+}
---
+++ Models/WorkTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CRMSystem.Models
+{
+    public class WorkTask
+    {
+        public int WorkTaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ResponsiblePerson { get; set; }
+        public string Status { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DeadLineDate { get; set; }
+    }
+}
